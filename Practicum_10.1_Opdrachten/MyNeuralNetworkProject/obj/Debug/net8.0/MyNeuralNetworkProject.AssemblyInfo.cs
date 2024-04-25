5cc54e9222b161547f56366ff36399429366631e
troykort <troykort@gmail.com>
2024-04-25 14:44:58 +0200

    N

--- Practicum_10.1_Opdrachten/MyNeuralNetworkProject/obj/Debug/net8.0/MyNeuralNetworkProject.AssemblyInfo.cs
+++ Practicum_10.1_Opdrachten/MyNeuralNetworkProject/obj/Debug/net8.0/MyNeuralNetworkProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNeuralNetworkProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9efff6e0666b85bf723521f0169b8cc43fcb7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4411cb6cb764ccffc0cf6ec84dfcc7dda92547")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNeuralNetworkProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNeuralNetworkProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
